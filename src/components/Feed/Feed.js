b668526456d4685dd9fa3587e92b1f2c489c97cd
CongFED <npc.it.dev@gmail.com>
2024-05-19 01:40:25 +0700

    update

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -10,6 +10,7 @@ import FontAwesome from "react-native-vector-icons/FontAwesome";
 import Feather from "react-native-vector-icons/Feather";
 import Octicons from "react-native-vector-icons/Octicons";
 import AntDesign from "react-native-vector-icons/AntDesign";
+import DropDownPicker from 'react-native-dropdown-picker';
 import Sound from 'react-native-sound';
 const { height: windowHeight } = Dimensions.get('window');
 const { width: windowWidth } = Dimensions.get('window');
@@ -20,14 +21,18 @@ const Feed = ({data}) => {
   const [idUserR, setidUsersR] = useRecoilState(idUsers);
   const [isUpdatePostR, setSsUpdatePost] = useRecoilState(isUpdatePost);
   const [idUser,setIdUser] = useState("")
+  const [content,setContent] = useState("")
   const [visible, setVisible] = useState(false);
+  const [visibleShare, setVisibleShare] = useState(false);
+  const [dataInfo, setDataInfo] = useState([]);
   const navigation = useNavigation();
   useEffect(() => {
     setAuthToken(to);
       const fetchInfo = async () => {
         try {
           const responseInfor = await api.get('https://socialnetwork.somee.com/api/infor/myinfor');
-          // console.log("Info",responseInfor.data.data.userId)
+           console.log("Info",responseInfor.data.data)
+          setDataInfo(responseInfor.data.data)
           setIdUser(responseInfor.data.data.userId)
         }catch (e) {
           console.log(e)
@@ -95,134 +100,53 @@ const Feed = ({data}) => {
       })
       .catch((err) => console.log(err));
   };
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState("1");
+  const [items, setItems] = useState([
+    { label: 'Công khai', value: '1' },
+    { label: 'Bạn bè', value: '2' },
+  ]);
+  console.log("SSS", value, items)
+  const handleShare = async () => {
+    try {
+      // setLike(!like);
+      // setCountData(data.countLike + 1);
+
+
+      await api
+        .post(
+          "https://socialnetwork.somee.com/api/post/share",
+          {
+            PostId: data.id,
+            LevelView: value,
+            content: content,
+          },
+          {
+            headers: {
+              "Content-Type": "multipart/form-data",
+            },
+          }
+        )
+        .then((response) => {
+          // Cập nhật dữ liệu vào state
+          console.log(response);
+          if (response.status == 200) {
+              setVisibleShare(false)
+            // dispatch(fetchPost());
+            // setLike(like + 1);
+            // setCountData(data.countLike - 1);
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    } catch (error) {
+      console.error("Login failed", error);
+    }
+  };
+  console.log(data)
   return (
-    <View style={styles.container}>
-      <View style={styles.headerWrapper}>
-        <TouchableOpacity style={styles.headerLeftWrapper} onPress={handleNavigate}>
-          <Image
-            style={styles.profileThumb}
-            source={{uri: data.avatarUrl}}
-          />
-          <Text style={styles.headerTitle}> {data.fullName}</Text>
-        </TouchableOpacity>
-        {
-          idUser === data.userId && ( <View style={{display:"flex", flexDirection:"row", justifyContent:"center", alignItems:"center"}}>
-          <TouchableOpacity style={styles.headerLeftWrapper}>
-          <AntDesign name="edit" size={20} color="#456fe6" style={{marginRight:10}}/>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.headerLeftWrapper} onPress={hanldDltPost}>
-        <AntDesign name="closecircleo" size={20} color="#456fe6" style={{marginRight:10}}/>
-      </TouchableOpacity>
-          </View>)
-        }
-       
-      </View>
-      <Text style={{marginBottom:10, paddingLeft:10}}>
-          {' '}
-          <Text style={styles.headerTitle}>{data.content}</Text>{' '}
-
-        </Text>
-      <View>
-      {
-        data.images.length > 1 ? <View style={{display:"flex", flexDirection:"row",justifyContent:"center", alignItems:"center"}}>
-        {
-          data.images.map((item, index) => (
-           
-            
-           
-          
-      
-            <TouchableOpacity onPress={() => handleImage(item)} key={index} >
-        <Image
-          style={{ height:300, width:200, flex:1 }}
-          resizeMode="cover"
-          source={{ uri: item.linkImage }}
-        />
-        <Modal
-        transparent={true}
-        visible={visible}
-        onRequestClose={() => setVisible(false)}
-      >
-        <TouchableOpacity style={styles.modalBackground} onPress={() => setVisible(false)}>
-        <Image
-        style={{ width:windowWidth * 0.8, height:"auto", aspectRatio: 1 }}
-        resizeMode="contain"
-        source={{ uri: imageBig }}
-      />
-        </TouchableOpacity>
-      </Modal>
-      </TouchableOpacity>
-          ))
-        }
-        </View>  : <View>
-        {
-          data.images.map((item, index) => (
-           
-            
-           
-          
-      
-            <TouchableOpacity onPress={() => handleImage(item.linkImage)} key={index}>
-        <Image
-          style={{ flex: 1, aspectRatio: 1 }}
-          resizeMode="cover"
-          source={{ uri: item.linkImage }}
-        />
-        <Modal
-        transparent={true}
-        visible={visible}
-        onRequestClose={() => setVisible(false)}
-      >
-        <TouchableOpacity style={styles.modalBackground} onPress={() => setVisible(false)}>
-        <Image
-        style={{ width:windowWidth * 1, height:"auto", aspectRatio: 1 }}
-        resizeMode="contain"
-        source={{ uri: item.linkImage }}
-      />
-        </TouchableOpacity>
-      </Modal>
-      </TouchableOpacity>
-          ))
-        }
-        </View> 
-
-      }
-     
-      
-      </View>
-      <View style={styles.feedImageFooter}>
-        <View style={styles.feddimageFooterLeftWrapper}>
-        <TouchableOpacity onPress={handleLike}>
-        <Feather name="heart" size={30} color="pink" style={{marginRight:10}}/>
-      </TouchableOpacity>
-          
-      <TouchableOpacity
-      style={styles.container1}
-      onPress={handleCmt}
-    >
-    <Octicons name="comment" size={30} color="#456fe6" style={{marginRight:10}}/>
-       </TouchableOpacity>
-         
-        </View>
-        <AntDesign name="sharealt" size={30} color="#456fe6" style={{marginRight:10}}/>
-      </View>
-      <View style={styles.underLineWRapper}>
-        <View style={styles.underLine} />
-      </View>
-      <View style={styles.likesAndCommentsWrapper}>
-      <Feather name="heart" size={20} color="pink" style={{marginRight:10}}/>
-        <Text style={styles.likesTitle}> {data.countLike}  Likes</Text>
-
-      
-      </View>
-      <View style={styles.likesAndCommentsWrapper}>
-      
-    
-
-      
-      </View>
- 
-    </View>
+      <View></View>
   );
 };
 
@@ -242,9 +166,9 @@ export const styles = StyleSheet.create({
   },
   headerWrapper: {
     display: 'flex',
-    flexDirection: 'row',
+    flexDirection: 'column',
     justifyContent: 'space-between',
-    padding: 10,
+  
   },
   input: {
     flex: 1,
@@ -261,6 +185,17 @@ export const styles = StyleSheet.create({
   headerLeftWrapper: {
     flexDirection: 'row',
     alignItems: 'center',
+    paddingHorizontal: 10,
+  },
+  headerLeftWrapper1: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 10,
+  },
+  headerLeftWrapper2: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent:"flex-end"
   },
   headerTitle: {
     fontSize: 18,
@@ -323,5 +258,23 @@ export const styles = StyleSheet.create({
     shadowRadius: 2,
     elevation: 1,
   },
-  
+  input: {
+    flex: 1,
+   
+    paddingVertical:20,
+    height:"80%"
+  },
+  containerS: {
+    flex: 1,
+    justifyContent: 'center',
+    height:100,
+
+
+  },
+  dropdown: {
+
+   padding:0,
+   
+  },
+
 });
